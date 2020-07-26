41876ee6f35dd18a54293ddc76f2af9f3dfc8d15
zygmunman <jmart237@gmail.com>
2020-07-26 17:33:13 +0200

    Vídeo 16: Sistema de menús IV

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -64,8 +64,9 @@
     <!-- FastClick -->
     <script src="{{asset("assets/$theme/bower_components/fastclick/lib/fastclick.js")}}"></script>
     <!-- AdminLTE App -->
-    <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
-
+    <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
+    <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+    <script src="{{asset("assets/js/funciones.js")}}"></script>
     @yield('scripts')
 
     </body>
