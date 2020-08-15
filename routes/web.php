40d3a78f6a24d28f793a98d987891beb1f039086
zygmunman <jmart237@gmail.com>
2020-08-15 12:57:07 +0200

    Vídeos 30-31: Sistema de Permisos finalizado

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,18 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' =>['auth
 
     //RUTAS DE MENU-ROL
     Route::get('menu-rol', 'MenuRolController@index')->name('menu-rol');
-    Route::post('menu-rol', 'MenuRolController@guardar')->name('guardar_menu_rol');      
+    Route::post('menu-rol', 'MenuRolController@guardar')->name('guardar_menu_rol'); 
+    
+    /*RUTAS PERMISO_ROL*/
+    Route::get('permiso-rol', 'PermisoRolController@index')->name('permiso_rol');
+    Route::post('permiso-rol', 'PermisoRolController@guardar')->name('guardar_permiso_rol');
 
 });
+
+ //RUTAS DE LIBRO
+Route::get('libro', 'LibroController@index')->name('libro');
+Route::get('libro/crear', 'LibroController@crear')->name('crear_libro');
+Route::post('libro', 'LibroController@guardar')->name('guardar_libro');
+Route::get('libro/{id}/editar', 'LibroController@editar')->name('editar_libro');
+Route::put('libro/{id}', 'LibroController@actualizar')->name('actualizar_libro');
+Route::delete('libro/{id}', 'LibroController@eliminar')->name('eliminar_libro');
